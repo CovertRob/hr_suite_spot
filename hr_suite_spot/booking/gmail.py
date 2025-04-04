183d5de833ea1e275ea2c0943cfbc7716c2843c5
Robert Feconda <robby.feconda365@gmail.com>
2025-04-05 01:52:19 +1000

    Fix up for integration.

--- hr_suite_spot/booking/gmail.py
+++ hr_suite_spot/booking/gmail.py
@@ -8,57 +8,77 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from google.oauth2 import service_account
 import logging
-
+import os
 
 logger = logging.getLogger('__name__')
 
-# Path to your Service Account JSON key
-SERVICE_ACCOUNT_FILE = Path("./booking/hr-suite-spot-8164bd363e86.json")
+class GmailIntegration:
 
-SCOPES = ['https://mail.google.com/']
+    SCOPES = ['https://mail.google.com/']
 
-BUSINESS_EMAIL = 'contact@hrsuitespot.com'
+    BUSINESS_EMAIL = 'cto@hrsuitespot.com'
 
-def create_message_with_attachment(to, from_email, subject, body, file_path):
-    message = MIMEMultipart()
-    message['to'] = to
-    message['from'] = from_email
-    message['subject'] = subject
+    def __init__(self):
+        self._api_key_path = self._find_api_key()
+        self.service = self._authorize()
 
-    # Add plain text part
-    message.attach(MIMEText(body, 'plain'))
+    @property
+    def get_api_key_path(self):
+        return self._api_key_path
 
-    # Add attachment
-    with open(file_path, 'rb') as f:
-        part = MIMEBase('application', 'pdf')
-        part.set_payload(f.read())
-        encoders.encode_base64(part)
-        part.add_header('Content-Disposition', 'attachment', filename='resume_guide.pdf')
-        message.attach(part)
 
-    # Encode to base64 for Gmail API
-    raw = base64.urlsafe_b64encode(message.as_bytes()).decode()
-    return {'raw': raw}
+    @staticmethod
+    def _find_api_key() -> str:
+        """
+        Since Credentials.from_service_acccount_file() takes file path, find the file path to either the environment variable in prod or local dev file.
+        """
+        api_key_path = os.getenv('SERVICE_ACCOUNT_FILE')
+        # If none, then get local development key
+        if not api_key_path:
+            api_key_path = Path("./booking/hr-suite-spot-8164bd363e86.json")
+        return api_key_path
+    
 
-def send_email():
-    try:
-        service = _authorize()
-        message = create_message_with_attachment('rfsuzuki85@gmail.com', 'contact@hrsuitespot.com', 'Test', 'Test email via gmail API', Path('./static/products/salary_negotiation_guide.pdf'))
-        send_message = (service.users().messages().send(userId='me', body=message).execute())
-    except HttpError as e:
-        logger.error(f'An error occurred: {e}')
-        send_message = None
-    return send_message
-        
+    def create_message_with_attachment(self, to, from_email, subject, body, file_path):
+        message = MIMEMultipart()
+        message['to'] = to
+        message['from'] = from_email
+        message['subject'] = subject
 
-def _authorize():
-    # print(SERVICE_ACCOUNT_FILE)
-    creds = service_account.Credentials.from_service_account_file(
-            SERVICE_ACCOUNT_FILE.absolute(),
-            scopes=SCOPES,
-            subject=BUSINESS_EMAIL  # Impersonating the business email
-        )
-    return build("gmail", "v1", credentials=creds)
+        # Add plain text part
+        message.attach(MIMEText(body, 'plain'))
+
+        # Add attachment
+        if file_path is not None:
+            with open(file_path, 'rb') as f:
+                part = MIMEBase('application', 'pdf')
+                part.set_payload(f.read())
+                encoders.encode_base64(part)
+                part.add_header('Content-Disposition', 'attachment', filename='resume_guide.pdf')
+                message.attach(part)
+
+        # Encode to base64 for Gmail API
+        raw = base64.urlsafe_b64encode(message.as_bytes()).decode()
+        return {'raw': raw}
+
+    # Attachment to remain None until document uploading implemented
+    def send_email(self, name: str, email: str, body: str, phone: str, attachment: Path=None):
+        try:
+            message = self.create_message_with_attachment('contact_submissions@hrsuitespot.com', 'cto@hrsuitespot.com', f'Contact From: {name} {phone} {email}', body, attachment)
+            send_message = (self.service.users().messages().send(userId='me', body=message).execute())
+        except HttpError as e:
+            logger.error(f'An error occurred: {e}')
+            send_message = None
+        return send_message
+            
+
+    def _authorize(self):
+        creds = service_account.Credentials.from_service_account_file(
+                self.get_api_key_path.absolute(),
+                scopes=self.SCOPES,
+                subject=self.BUSINESS_EMAIL  # Impersonating the business email
+            )
+        return build("gmail", "v1", credentials=creds)
 
 if __name__ == '__main__':
-    send_email()
+    pass
